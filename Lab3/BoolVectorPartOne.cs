ac2aec82566575dc61301f930d8ef2e70909b237
Andrey Ostrikov <andrey.ostrikov.bestu@mail.ru>
2021-01-02 23:06:05 +0300

    Добавьте файлы проекта.

---
+++ Lab3/BoolVectorPartOne.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    /**
+     * Определяем класс по варианту.
+     * Сделаем его partial, например, чтобы разбить на несколько файлов.
+     * При компиляции, классы отмеченные partial будут собраны в один.
+     */
+    partial class BoolVector
+    {
+        /// 
+        /// Создадим блок private полей, как сказано в задании.
+        ///
+        // Поле только для чтения, содержащее уникальный хеш.
+        private readonly int ID;
+
+        // Константное поле класса.
+        private const string CLASS_NAME = "BoolVector";
+
+        // Статическое поле хранящее количество созданных объектов.
+        private static int objectCounter = 0;
+
+        // Поле, хранит заданный вектор логических элементов.
+        private List<bool> _vector;
+
+        /// 
+        /// Создадим блок свойств, для работы с полями класса.
+        ///  
+        // Свойство, выводящее уникальный хеш объекта.
+        // Поскольку поле readonly, от модификатора set нет смысла.
+        public int Id
+        {
+            get { return ID; }
+        }
+
+        // Свойство, выводящее константное поле.
+        // Заменить, поскольку поле константно, от модификатора set нет смысла.
+        public string Name
+        {
+            get { return CLASS_NAME; }
+        }
+
+        // Свойство, для получения значения количества созданных объектов класса.
+        public int Counter
+        {
+            get { return objectCounter; }
+        }
+
+        // Свойство для получения вектора и для присваивания полю вектора.
+        public List<bool> Vector
+        {
+            get { return _vector; }
+            set { _vector = value; }
+        }
+
+        /// 
+        /// Блок переопределения стандартных object методов.
+        /// ToString, Equals, GetHashCode
+        /// Варианты тестовых реализаций.
+        /// 
+        public override string ToString()
+        {
+            return $"**Call override ToString()**" +
+                $"\nHash: {ID}\nClassName: {CLASS_NAME}\n";
+        }
+
+        public override int GetHashCode()
+        {
+            return objectCounter.GetHashCode() ^
+                CLASS_NAME.GetHashCode() ^
+                ID.GetHashCode() + 50;
+        }
+
+        public override bool Equals(object obj)
+        {
+            List<bool> vector;
+            try
+            {
+                // Вектор, который хотим сравнить. Делаем преобразование к заданному типу.
+                vector = (List<bool>)obj;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Невозможно преобразовать тип.");
+            }
+
+            // Выполняем сравнение и возвращаем результат.
+            return _vector.SequenceEqual(vector);
+        }
+
+        /// 
+        /// Создадим методы, для указанных действий.
+        /// 
+        /**
+         * Конъюнкция
+         * true  & true  = true
+         * true  & false = false
+         * false & false = false
+         * false & true  = false
+         */
+        public void Conjunction(bool logic)
+        {
+            var v = _vector.Select(x => x & logic).ToList();
+            foreach (var item in v)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+        /**
+         * Дизъюнкция
+         * false | false = false
+         * true  | true  = true
+         * true  | false = true
+         * false | true  = true
+         */
+        public void Disjunction(bool logic)
+        {
+            var v = _vector.Select(x => x | logic).ToList();
+            foreach (var item in v)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+        /**
+         * Отрицание
+         * !false = true
+         * !true  = false
+         */
+        public void Negative()
+        {
+            var v = _vector.Select(x => !x).ToList();
+            foreach (var item in v)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+        // Подсчет единиц.
+        public int CalculateZero()
+        {
+            return _vector.Where(x => x == true).Count();
+        }
+
+        // Подсчет нулей, результат будем возвращать с помощью out.
+        public void CalculateNull(out int result)
+        {
+            result = _vector.Where(x => x == false).Count();
+        }
+
+        // Статический метод для вывода информации о классе.
+        public static void ClassInfo(ref int t)
+        {
+            t = 23232;
+            Console.WriteLine("**Вызван статический метод информации о классе**\n" +
+                "класс Булев вектор (BoolVector).\n" +
+                "Реализовать методы для выполнения поразрядных  конъюнкции,\n" +
+                "дизъюнкции  и отрицания  векторов, а также подсчета числа\n" +
+                "единиц и нулей в векторе.");
+        }
+    }
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = 0;
+            /**
+             * Протестируем созданный класс, вызовами методов, свойств итд...
+             */
+            /**
+             * По заданию требовалось передать что-то по ссылке, для одного из методов класса.
+             * При передаче по ссылке, не создается копия объекта,
+             * поэтому значение измененное в методе, изменится и в месте вызова.
+             */
+            BoolVector.ClassInfo(ref t); // Информация о классе.
+            BoolVector instance1 = new BoolVector();
+            Console.WriteLine("\n");
+
+            BoolVector instance2 = new BoolVector(new List<bool> { true, false, true, false, true });
+
+            Console.WriteLine("Базовый вектор, без изменений:");
+            for (int i = 0; i < instance2.Vector.Count; i++)
+                Console.Write($"{instance2.Vector[i]} ");
+
+            Console.WriteLine("\nРезультат после конъюнкции по значению false:");
+            instance2.Conjunction(false);
+
+            Console.WriteLine("\nРезультат после дизъюнкции по значению true:");
+            instance2.Disjunction(true);
+
+            Console.WriteLine("\nРезультат после отрицания:");
+            instance2.Negative();
+
+            int result = 0;
+            instance2.CalculateNull(out result);
+            Console.WriteLine($"\nКоличество нулей в векторе: {result}\n" +
+                $"Количество единиц в векторе: {instance2.CalculateZero()}");
+
+            Console.WriteLine($"Количество созданных объектов класса: {instance2.Counter}");
+            Console.WriteLine("\n\n");
+
+            // Создадим список объектов.
+            List<BoolVector> boolVectors = new List<BoolVector>()
+            {
+                new BoolVector(new List<bool> { true, false, false, false, false }),
+                new BoolVector(new List<bool> { true, false, false, false, false }),
+                new BoolVector(new List<bool> { true, false }),
+                new BoolVector(new List<bool> { true, false }),
+                new BoolVector(new List<bool> { false, false }),
+                new BoolVector(new List<bool> { true, false, true }),
+                new BoolVector(new List<bool> { true, false })
+            };
+
+            // a) Выводим на консоль вектора
+            foreach (var item in boolVectors)
+            {
+                Console.Write($"Хеш вектора: {item.Id}, содержимое вектора: ");
+                for (int i = 0; i < item.Vector.Count; i++)
+                {
+                    Console.Write($"{item.Vector[i]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+
+            // a) Зададим числа и попробуем найти соответствия
+            int desireNullNumber = 4;
+            int desireZeroNumber = 2;
+            foreach (var item in boolVectors)
+            {
+                // Возвращать значение будем с помощью out, по требованию в задании.
+                item.CalculateNull(out int res2);
+                if (res2 == desireNullNumber)
+                {
+                    Console.WriteLine("Вектор с заданным числом нулей найден" +
+                        $" его хеш {item.Id}");
+                }
+                if (item.CalculateZero() == desireZeroNumber)
+                {
+                    Console.WriteLine("Вектор с заданным числом единиц найден" +
+                        $" его хеш {item.Id}");
+                }
+            }
+
+            // b) Ищем равные вектора перебором и выводим их.
+            for (int i = 0; i < boolVectors.Count; ++i)
+            {
+                for (int j = 1; j < boolVectors.Count - 1; ++j)
+                {
+                    if (i != j) // Пропускаем текущий, чтобы исключить случай, когда вектор равен сам себе.
+                    {
+                        // Вызываем переопределенный equals
+                        if (boolVectors[i].Equals(boolVectors[j].Vector))
+                        {
+                            Console.WriteLine($"Вектор {boolVectors[i].Id} равен вектору {boolVectors[j].Id}");
+                        }
+                    }
+                }
+            }
+
+            // Анонимный тип. Нужны для получения каких-нибудь данных,
+            // и преобразовывания во что-то для дальнейшей обработки.
+            // Создадим тестовый анонимный тип.
+            var test = new { Id = 12334, Name = "test", Counter = 34 };
+            // Теперь вызовем свойства анонимного типа.
+            Console.WriteLine($"\nВызов анонимного типа {test.Id}," +
+                $"{test.Name}, {test.Counter}");
+
+            Console.ReadKey();
+        }
+    }
+}
